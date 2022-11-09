ead31ce5af76f64a7e3ca0a8704b6cbaa6b9e0ef
Ahmed Rajib <ahmedrajibbb@gmail.com>
2022-11-09 13:51:24 +0600

    Initial Commit

---
+++ GridViewCustomDesign/GridViewCustomDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridViewCustomDesignApp.swift
+//  GridViewCustomDesign
+//
+//  Created by Ahmed Rajib on 9/11/22.
+//
+
+import SwiftUI
+
+@main
+struct GridViewCustomDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
