c913674e2d1bb48ba66ac41a68d61509d02483c2
Ahmed Rajib <ahmedrajibbb@gmail.com>
2022-11-09 18:45:05 +0600

    GridViewDesign

--- GridViewCustomDesign/ContentView.swift
+++ GridViewCustomDesign/ContentView.swift
@@ -8,12 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private var symbols = ["keyboard", "hifispeaker.fill", "printer.fill", "tv.fill", "desktopcomputer", "headphones", "tv.music.note", "mic", "plus.bubble", "video"]
+    
+    private var colors: [Color] = [.yellow, .purple, .green]
+    
+    private var gridItemLayout = [GridItem(.flexible())]
+    private var gridItemLayout4 = [GridItem(.flexible()),GridItem(.flexible())]
+    let resultGridLayout = [
+        GridItem(.adaptive(minimum: 160), spacing: 10,alignment: .top),
+        GridItem(.adaptive(minimum: 160), spacing: 10,alignment: .bottom),
+        GridItem(.adaptive(minimum: 160), spacing: 10,alignment: .leading),
+        GridItem(.adaptive(minimum: 160), spacing: 10,alignment: .trailing)
+    ]
+    
+    private var gridItemLayout2 = [
+        GridItem(.adaptive(minimum: 10,maximum: 12)),
+    ]
+    private var fullscreenLayout = [GridItem(.flexible())]
+    
+    let arr = [1,1,1,4,4,1,1,1,8,8,8]
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ScrollView() {
+            ForEach(arr,id: \.self) { item in
+                if item == 1 {
+                    LazyVGrid(columns: gridItemLayout2) {
+                        LazyVGrid(columns: resultGridLayout) {
+                            ForEach(0...0,id: \.self) { item in
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 0, style: .continuous)
+                                        .fill(.blue)
+                                        .frame(width: 30, height: 30)
+                                        .padding(.horizontal, 8)
+                                }
+                            }
+                        }
+                    }
+                }else if item == 4 {
+                    LazyVGrid(columns: gridItemLayout4) {
+                        ForEach(0...1, id:\.self) { item in
+                            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                                .fill(.red)
+                                .frame(width: UIScreen.screenWidth / 2.2, height: 100)
+                                .padding(.horizontal, 8)
+                        }
+                    }
+                }else if item == 8{
+                    LazyVGrid(columns: gridItemLayout) {
+                        RoundedRectangle(cornerRadius: 25, style: .continuous)
+                            .fill(.black)
+                            .frame(width: UIScreen.screenWidth - 40, height: 100)
+                            .padding(.horizontal, 8)
+                    }
+                }
+            }
         }
         .padding()
     }
---
+++ GridViewCustomDesign/UIScreen+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIScreen + Extension.swift
+//  TokyoNavi
+//
+//  Created by MacBook Pro on 25/9/22.
+//
+
+import SwiftUI
+
+extension UIScreen{
+    static let screenWidth = UIScreen.main.bounds.size.width
+    static let screenHeight = UIScreen.main.bounds.size.height
+    static let screenSize = UIScreen.main.bounds.size
+    static var sideBarWidth = UIScreen.main.bounds.size.width * 0.8
+}
